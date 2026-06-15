f0d394f2eaa1e16813a221bdb82b51992188e3b2
mayaarafat2233 <162817245+mayaarafat2233@users.noreply.github.com>
2026-06-15 17:13:59 -0500

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Dashboard } from './Dashboard'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Dashboard />
-  </React.StrictMode>,
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
-
-
